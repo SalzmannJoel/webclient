d55f51994e689f783ccf93f869af984541fdc377
Joel Salzmann <salzmann.joel@gmail.com>
2018-04-06 16:15:46 +0200

    implemented almost empty statePrinter

--- stateprinter/statePrinter.js
+++ stateprinter/statePrinter.js
@@ -1,7 +1,13 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-
+class StatePrinter extends StateChangeListener {
+    constructor() {
+        super();
+        this.view = new StatePrinterView();
+        this.model = new StatePrinterModel();
+    }
+    
+    stateChanged(state) {
+        this.view.printState(state);
+    }
+    
+}
--- stateprinter/statePrinterModel.js
+++ stateprinter/statePrinterModel.js
@@ -1,7 +1,23 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-
+class StatePrinterModel extends StateUser {
+    constructor() {
+        super();
+        this.stateChangeListeners = [];
+    }
+    
+    registerStateChangeListener(obj) {
+        if(obj instanceof StateChangeListener || obj instanceof PositionAndStateChangeListener) {
+            this.stateChangeListeners.push(obj);
+            console.log("New stateChangeListener in LogPrinterModel" + obj);
+        }
+    }
+    
+    setState(state) {
+        if(state > 0 && state <8) {
+            this.state = state;
+            this.stateChangeListeners.forEach(function(item) {
+                item.stateChanged(state);
+            });
+        }
+    }
+}
