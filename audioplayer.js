f8b62a7892a396cc1716e6ec09bcae3754359012
iasalzma <joel.salzmann@stud.hslu.ch>
2018-05-31 15:18:33 +0200

    added some more audios

--- audioplayer.js
+++ audioplayer.js
@@ -10,9 +10,25 @@ class Audioplayer extends InterfaceStateUser {
             switch(state) {
                 case State['DEVICE_STARTED']:
                     document.getElementById("audioItsMe").play();
-                    document.getElementById("audioMain").play();
+                    setTimeout(function(){ document.getElementById("audioMain").play(); }, 1000);              
+                    break;
+                case State['OBSTACLE_PASSED']:
+                    document.getElementById("audioMain").pause();
+                    document.getElementById("audioHereWeGo").play();
+                    setTimeout(function(){ document.getElementById("audioMain").play(); }, 1000);
+                    break;
+                case State['PACKAGE_PICKED_UP']:
+                    document.getElementById("audioMain").pause();
+                    document.getElementById("audioPowerUp").play();
+                    setTimeout(function(){ document.getElementById("audioMain").play(); }, 1000);
+                    break;
+                case State['PACKAGE_DROPPED']:
+                    document.getElementById("audioMain").pause();
+                    document.getElementById("audioMamamia").play();
+                    setTimeout(function(){ document.getElementById("audioMain").play(); }, 1000);
                     break;
                 case State['DESTINATION_REACHED']:
+                    document.getElementById("audioMain").pause();
                     document.getElementById("audioTheEnd").play();
                     break;
             }
